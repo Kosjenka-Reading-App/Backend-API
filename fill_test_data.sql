090fd918efaa83eeabde5d58ae383369dc04d6a7
Ambre Simond <ambre.simond@grenoble-inp.org>
2023-11-03 15:46:19 +0100

    add missing email

--- fill_test_data.sql
+++ fill_test_data.sql
@@ -1,3 +1,5 @@
+delete from exercise;
+delete from account;
 insert into exercise (id,title,text,complexity) values
     (1,"Tom the cat","Tom the cat was a nice fellow. Shame that he passed his entrance exams poorly",2.0),
     (2,"Matt the cat","Matt the cat was a nice fellow. Shame that he passed his entrance exams well",2.5),
@@ -5,7 +7,7 @@ insert into exercise (id,title,text,complexity) values
     (4,"Brian the dog","Brian like jokes. Brian go woof-woof,hooman go ha-ha. Brian like stick. Brian chase stick. Brian ean stick.",1.5),
     (5,"Finn the human","Finn is unimagenably sad and stressed. Finn's got a thesis to write. Finn doesn't have a thesis advisor.",9000.0);
 
-insert into account (id_account,password,is_user,is_super_admin) values
+insert into account (id_account,email,password,is_user,is_super_admin) values
     (1,"a@gmail.com","qwerty",true,false),
     (2,"b@gmail.com","asdfgh",true,false),
     (3,"c@gmail.com","123456",false,false),
