d5100c4ca8aa87b85bc91bf14df626cf6772d725
Ambre Simond <ambre.simond@grenoble-inp.org>
2023-11-03 15:47:44 +0100

    add test create account

--- test_api.py
+++ test_api.py
@@ -85,3 +85,15 @@ class TestAPI:
             assert len(remaining_exercise_ids) == len(exercise_ids)
             assert exercise_id not in remaining_exercise_ids
 
+    def test_create_account(self):
+        accounts = requests.get('http://localhost:8000/accounts').json()     
+        account_count = len(accounts)
+        new_account = {
+            'email': 'email@gmail.com',
+            'password': 'secret',
+            'is_user': True,
+            'is_super_admin' : False
+        }
+        created_account = requests.post('http://localhost:8000/accounts', json=new_account).json()
+        accounts = requests.get('http://localhost:8000/accounts').json()
+        assert len(accounts) == account_count + 1
