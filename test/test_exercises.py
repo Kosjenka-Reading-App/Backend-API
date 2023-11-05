9b98d5c9631d31c04753bb2914e093e58d2428cf
André Moreira <andre.moreira.9@hotmail.com>
2023-11-05 17:28:29 +0100

    Fix test pipeline

--- test/test_exercises.py
+++ test/test_exercises.py
@@ -9,7 +9,8 @@ def test_create_exercise():
         'complexity': 3.0,
         'text': 'Text of the exercise'
     }
-    created_exercise = client.post('http://localhost:8000/exercises', json=new_exercise).json()
+    created_exercise = client.post(
+        'http://localhost:8000/exercises', json=new_exercise).json()
     for key in new_exercise:
         assert created_exercise[key] == new_exercise[key]
     exercises = client.get('http://localhost:8000/exercises').json()
@@ -23,7 +24,8 @@ def test_create_exercise_without_complexity():
         'title': 'Title of another exercise',
         'text': 'Text of another exercise'
     }
-    created_exercise = client.post('http://localhost:8000/exercises', json=new_exercise).json()
+    created_exercise = client.post(
+        'http://localhost:8000/exercises', json=new_exercise).json()
     for key in new_exercise:
         assert created_exercise[key] == new_exercise[key]
     assert created_exercise['complexity'] == 0.0
@@ -39,17 +41,21 @@ def test_get_exercises():
 def test_get_exercise():
     exercises = client.get('http://localhost:8000/exercises').json()
     exercise_id = exercises[0]['id']
-    exercise = client.get(f'http://localhost:8000/exercises/{exercise_id}').json()
+    exercise = client.get(
+        f'http://localhost:8000/exercises/{exercise_id}').json()
     assert set(exercise.keys()) == {'id', 'title', 'complexity', 'text'}
 
 
 def test_update_exercise():
     exercises = client.get('http://localhost:8000/exercises').json()
     exercise_id = exercises[0]['id']
-    original_exercise = client.get(f'http://localhost:8000/exercises/{exercise_id}').json()
+    original_exercise = client.get(
+        f'http://localhost:8000/exercises/{exercise_id}').json()
     body = {'title': 'Updated title'}
-    client.patch(f'http://localhost:8000/exercises/{exercise_id}', json=body).json()
-    updated_exercise = client.get(f'http://localhost:8000/exercises/{exercise_id}').json()
+    client.patch(
+        f'http://localhost:8000/exercises/{exercise_id}', json=body).json()
+    updated_exercise = client.get(
+        f'http://localhost:8000/exercises/{exercise_id}').json()
     for key in updated_exercise:
         if key == 'title':
             assert updated_exercise[key] == 'Updated title'
@@ -64,7 +70,7 @@ def test_delete_exercise():
     while exercise_ids:
         exercise_id = exercise_ids.pop()
         client.delete(f'http://localhost:8000/exercises/{exercise_id}').json()
-        remaining_exercise_ids = {ex['id'] for ex in client.get('http://localhost:8000/exercises').json()}
+        remaining_exercise_ids = {ex['id'] for ex in client.get(
+            'http://localhost:8000/exercises').json()}
         assert len(remaining_exercise_ids) == len(exercise_ids)
         assert exercise_id not in remaining_exercise_ids
-
