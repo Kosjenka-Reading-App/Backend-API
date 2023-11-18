65711adb3f6053f39a4642d7190ff5cb0fc187c3
vvihorev <vvihorevv@gmail.com>
2023-11-18 12:26:28 +0100

    cleanup sqlalchemy console

--- test/sqlalchemy_console.py
+++ test/sqlalchemy_console.py
@@ -16,25 +16,3 @@ Base = declarative_base()
 models.Base.metadata.create_all(bind=engine)
 db = SessionLocal()
 
-
-# NOTES ON WORK IN PROGRESS
-db_exercise = (
-    db.query(models.DoExercise)
-    .select_from(models.Exercise)
-    .join(models.Exercise.users)
-    .filter(models.Exercise.id == 1)
-    .filter(models.DoExercise.user_id == 1)
-    .add_entity(models.Exercise)
-    .first()
-)
-exercise_completion = schemas.ExerciseCompletion.model_validate(db_exercise[0])
-exercise = schemas.FullExerciseResponse.model_validate(db_exercise[1])
-exercise.completion = exercise_completion
-
-db_exercises = (
-    db.query(models.Exercise)
-    .join(models.DoExercise, isouter=True)
-    .add_entity(models.DoExercise)
-    .filter(or_(models.DoExercise.user_id == 1, models.Exercise.users == None))
-    .all()
-)
