32717c9217be61ecaab9becc7b4a0bd0773d8d6b
Ambre Simond <ambre.simond@grenoble-inp.org>
2023-11-24 16:54:17 +0100

    Fix test patch categories

--- test/test_categories.py
+++ test/test_categories.py
@@ -201,8 +201,8 @@ def test_delete_nonexistent_category(admin_token):
 
 def test_patch_nonexistent_category(admin_token):
     # Assume nonexistent_category doesn't exist
-    non_existent_category = "nonexistent_category"
-    body = {"category": ["cats", "mice"]}
+    non_existent_category = {"category": "non_existent_category"}
+    body = {"category": "cats"}
     resp = client.patch(
         f"http://localhost:8000/categories/{non_existent_category}",
         json=body,
