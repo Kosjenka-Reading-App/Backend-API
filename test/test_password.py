4f3e4d49e4432064732fe4fdb22a78c4f6dba321
Tim <tim.hosch@gmx.de>
2023-12-08 20:37:41 +0100

    Added password reset test

--- test/test_password.py
+++ test/test_password.py
@@ -1,6 +1,6 @@
 import pytest
 from conftest import client, auth_header
-
+from auth import createPasswortResetToken
 
 def test_send_password_mail_superadmin():
     email = {"email": "superadmin@gmail.com"}
@@ -28,20 +28,44 @@ def test_send_password_mail_user():
     )
 
 
-@pytest.mark.skip(
-    reason="we have to call function in auth.py to reset password directly"
-)
-def test_account_reset_password():
+def test_regular_account_reset_password():
     email = {"email": "regular@gmail.com"}
-    resp = client.post("http://localhost:8000/password/forgot", json=email)
+    resp = client.post(
+        "http://localhost:8000/password/forgot",
+        json=email,
+    )
     assert resp.status_code == 200
-    reset_request_result = resp.json()
-    assert reset_request_result["details"] == "Password reset email sent"
-    reset_token = reset_request_result["token"]
-    new_password_payload = {"password": "new_password", "token": reset_token}
+    assert (
+        "An email has been sent to regular@gmail.com with a link for password reset."
+        in resp.json()["result"]
+    )
+    reset_token = createPasswortResetToken("regular@gmail.com", 6000)
+    new_password_payload = {"password": "new_password_regular", "token": reset_token}
     reset_password_response = client.post(
         "http://localhost:8000/password/reset", json=new_password_payload
     )
     assert reset_password_response.status_code == 200
     reset_password_result = reset_password_response.json()
     assert reset_password_result["details"] == "Successfully updated password"
+   
+def test_superadmin_account_reset_password():
+    email = {"email": "superadmin@gmail.com"}
+    resp = client.post(
+        "http://localhost:8000/password/forgot",
+        json=email,
+    )
+    assert resp.status_code == 200
+    assert (
+        "An email has been sent to superadmin@gmail.com with a link for password reset."
+        in resp.json()["result"]
+    )
+    reset_token = createPasswortResetToken("superadmin@gmail.com", 6000)
+    new_password_payload = {"password": "new_password_superadmin", "token": reset_token}
+    reset_password_response = client.post(
+        "http://localhost:8000/password/reset", json=new_password_payload
+    )
+    assert reset_password_response.status_code == 200
+    reset_password_result = reset_password_response.json()
+    assert reset_password_result["details"] == "Successfully updated password"
+    
+   
