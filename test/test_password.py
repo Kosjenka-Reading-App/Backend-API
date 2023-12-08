eebcaa87cf392812d6512bfed80872fa13620511
Tim <tim.hosch@gmx.de>
2023-12-08 21:41:12 +0100

    formatted with black .

--- test/test_password.py
+++ test/test_password.py
@@ -2,6 +2,7 @@ import pytest
 from conftest import client, auth_header
 from auth import createPasswortResetToken
 
+
 def test_send_password_mail_superadmin():
     email = {"email": "superadmin@gmail.com"}
     resp = client.post(
@@ -47,11 +48,11 @@ def test_regular_account_reset_password():
     assert reset_password_response.status_code == 200
     reset_password_result = reset_password_response.json()
     assert reset_password_result["details"] == "Successfully updated password"
-    login_json = {"email" : "regular@gmail.com", "password" : "new_password_regular"}
-    login_respose = client.post("http://localhost:8000/login", json = login_json)
+    login_json = {"email": "regular@gmail.com", "password": "new_password_regular"}
+    login_respose = client.post("http://localhost:8000/login", json=login_json)
     assert login_respose.status_code == 200
-    
-    #Change password back, otherwise no login possible and a lots of test fail
+
+    # Change password back, otherwise no login possible and a lots of test fail
     new_password_payload = {"password": "regular", "token": reset_token}
     reset_password_response = client.post(
         "http://localhost:8000/password/reset", json=new_password_payload
@@ -59,9 +60,10 @@ def test_regular_account_reset_password():
     assert reset_password_response.status_code == 200
     reset_password_result = reset_password_response.json()
     assert reset_password_result["details"] == "Successfully updated password"
-    login_json = {"email" : "regular@gmail.com", "password" : "regular"}
-    login_respose = client.post("http://localhost:8000/login", json = login_json)
+    login_json = {"email": "regular@gmail.com", "password": "regular"}
+    login_respose = client.post("http://localhost:8000/login", json=login_json)
     assert login_respose.status_code == 200
+
 
 def test_superadmin_account_reset_password():
     email = {"email": "superadmin@gmail.com"}
@@ -82,12 +84,15 @@ def test_superadmin_account_reset_password():
     assert reset_password_response.status_code == 200
     reset_password_result = reset_password_response.json()
     assert reset_password_result["details"] == "Successfully updated password"
-    
-    login_json = {"email" : "superadmin@gmail.com", "password" : "new_password_superadmin"}
-    login_respose = client.post("http://localhost:8000/login", json = login_json)
+
+    login_json = {
+        "email": "superadmin@gmail.com",
+        "password": "new_password_superadmin",
+    }
+    login_respose = client.post("http://localhost:8000/login", json=login_json)
     assert login_respose.status_code == 200
-    
-    #Change password back, otherwise no login possible and a lots of test fail
+
+    # Change password back, otherwise no login possible and a lots of test fail
     new_password_payload = {"password": "superadmin", "token": reset_token}
     reset_password_response = client.post(
         "http://localhost:8000/password/reset", json=new_password_payload
@@ -95,6 +100,6 @@ def test_superadmin_account_reset_password():
     assert reset_password_response.status_code == 200
     reset_password_result = reset_password_response.json()
     assert reset_password_result["details"] == "Successfully updated password"
-    login_json = {"email" : "superadmin@gmail.com", "password" : "superadmin"}
-    login_respose = client.post("http://localhost:8000/login", json = login_json)
+    login_json = {"email": "superadmin@gmail.com", "password": "superadmin"}
+    login_respose = client.post("http://localhost:8000/login", json=login_json)
     assert login_respose.status_code == 200
